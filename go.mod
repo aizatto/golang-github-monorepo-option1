fefc8f215b0a5b0e1b076db4fc675b686ff9dff3
Aizat Faiz <aizat.faiz@gmail.com>
2021-07-25 00:07:14 +0800

    init

---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/aizatto/golang-github-monorepo-option1
+
+go 1.16
+
---
+++ pkg2/main.go
@@ -0,0 +1,9 @@
+package main
+
+import(
+	"github.com/aizatto/golang-github-monorepo-option1/pkg1/pkg/helloworld"
+)
+
+func main() {
+	helloworld.Helloworld()
+}
